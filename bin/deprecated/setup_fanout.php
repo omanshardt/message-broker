96080e3b7f5f832e5dee74d3d57c61393fa82e24
Ole Manshardt <mail@manshardt.de>
2025-12-23 23:13:51 +0100

    Move deprecated setup scripts to directory 'deprecated'.
    - Clean up deprecated setup scripts to work from new directory and to get rid of unneeded user prompts.

--- bin/deprecated/setup_fanout.php
+++ bin/deprecated/setup_fanout.php
@@ -1,25 +1,25 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 use PhpAmqpLib\Connection\AMQPConnectionFactory;
 use PhpAmqpLib\Message\AMQPMessage;
 
-$config = require __DIR__ . '/../config.php';
+$config = require __DIR__ . '/../../config.php';
 
 use App\Utils;
 
 echo "Setup one exchange and one queue and bind them to each other.\n";
 
+$exchange = 'ex:fanout';
 $handle = fopen("php://stdin", "r");
-$exchange = Utils::create_or_select_exchange($handle);
 $queue = Utils::create_or_select_queue($handle);
 
 try {
     $connection = AMQPConnectionFactory::create($config);
     $channel = $connection->channel();
 
-    $channel->exchange_declare('ex:fanout', 'fanout', false, false, false);
+    $channel->exchange_declare($exchange, 'fanout', false, false, false);
     $channel->queue_declare($queue, false, false, false, false);
     $channel->queue_bind($queue, $exchange);
 
